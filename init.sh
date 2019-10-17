58fa443edc6493a0ccdc24d601b9c9af4e00607f
Philipp Schmitt <philipp@schmitt.co>
2019-10-17 14:06:10 +0200

    Fix filename

--- init.sh
+++ init.sh
@@ -25,13 +25,13 @@ get_ssh_key() {
 }
 
 add_trusted_key() {
-  # ssh-keyscan -H git.comreset.io -p 2022 >> ~/.ssh/authorized_keys
-  cat > ~/.ssh/authorized_keys <<- "EOF"
+  # ssh-keyscan -H git.comreset.io -p 2022 >> ~/.ssh/known_hosts
+  cat > ~/.ssh/known_hosts <<- "EOF"
 |1|FOEbOeHQWYJvOEq6TuMCZeXpXBo=|p3PuYvNYbf5LNwzTeEjNdDBFl/s= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOaSAsJXaxKjhtNSA7rWPtmCJYn/r0v7Xm/ZpC+Jn+pf1xt7TH7PeC6iczgCwL2q7uh7vUi7Yn1vGr5qcQy1K9aTF8xLVKWUD+H4Kh77oUFJ0p8pcSCTXkRl3cvWLfSSOVE5kjJgN4eUvUORzcY6jCg8HGh4rJIPeKgpTBGut0eMBPB9IvgnqxnSHiYz7GSqW//1Xj49ffR3H2pRIxe5ij75+X0yOqws65zIKtoUHq+bCXkr4VdBRZaq7ukJZ1/K/feuC52fbpR2ut8O0XgzF2NMl5jeG1oMOQBfDeVjyCSw8AmGoYt9ahKCRlNAdVUwnFrQl6v6W5x3ptbsqBreSB
 |1|GzUn+Xv+mLRMNkkN1qL/QGnaxMY=|N4vLI9HYeQwKH1UlSi5uymEiidI= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL3hkYuoAvI66HTT/XRtHNPUWpGU0KP4Bz2VIJqMqguHLTeslXHihhuYzrIX2vyJ0C1O/ifKYxSq5fdN1wPeT6Q=
 |1|RHKQ3eKrdoxnn7YDR2DocUNZuPE=|vM9qvumVnYJdHtLSZ8dmItWkwUo= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmqpF91ZH16AVCk8wnWoe/vCkizh0nGmVy2F8dT290A
 EOF
-  chmod 600 ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/known_hosts
 }
 
 yadm_init() {
