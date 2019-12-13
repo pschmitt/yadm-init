3795ceebe72a8dd552ce649551b709cf154b1c0b
Philipp Schmitt <philipp@schmitt.co>
2019-12-13 15:15:58 +0100

    Only check the clone result when assessing failure

--- init.sh
+++ init.sh
@@ -146,8 +146,9 @@ yadm_init() {
     for url in "${urls[@]}"
     do
       if GIT_SSH_COMMAND="ssh -i ~/.ssh/id_yadm_init -F /dev/null" \
-        bash "$(__get_tmpdir)/yadm" clone -f --bootstrap "$url"
+        bash "$(__get_tmpdir)/yadm" clone -f "$url"
       then
+        "${HOME}/.config/yadm/bootstrap"
         break
       fi
     done
