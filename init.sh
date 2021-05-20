c1ac3897690f9fcf11c80d57a6e82d033c7e8c42
Philipp Schmitt <philipp@schmitt.co>
2021-05-20 14:59:04 +0200

    Upgrade all packages on termux before proceeding (really only apt update is required - but we might as well upgrade as well)

--- init.sh
+++ init.sh
@@ -25,6 +25,7 @@ query_passphrase() {
 install_deps() {
   if command -v termux-info >/dev/null
   then
+    pkg upgrade -y
     pkg install -y curl git openssh sshpass
   elif command -v apt >/dev/null
   then
